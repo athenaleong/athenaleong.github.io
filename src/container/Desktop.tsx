c0c8ed05662030f0bcce7fecf39f2c602bf8a0ee
athenaleong <athenaleong619@gmail.com>
2023-03-28 20:24:17 -0700

    initial setup

---
+++ src/container/Desktop.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Desktop = () => {
+    return (
+        <div className="w-screen h-screen bg-black">
+        <h1>Desktop</h1>
+        </div>
+    );
+};
+
+export default Desktop;
+
+interface DesktopProps {
+};
