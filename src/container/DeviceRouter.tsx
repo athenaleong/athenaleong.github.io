a83a149aaec840198d967d1c99b91ae1ba6630dc
athenaleong <athenaleong619@gmail.com>
2023-04-08 08:37:25 -0700

    Reverted back to preload image

--- src/container/DeviceRouter.tsx
+++ src/container/DeviceRouter.tsx
@@ -15,39 +15,39 @@ const DeviceRouter: React.FC<any> = () => {
 
     const [loading, setLoading] = useState(false);
 
-//     useEffect(() => {
+    useEffect(() => {
 
-//     const images = [
-//         '/experiences.png',
-//         '/about.png',
-//         '/about-hover.png',
-//         '/project.png',
-//         '/project-hover.png',
-//         '/anglerfish.png',
-//         '/anglerfish-hover.png',
-//         '/garden.png',
-//         '/garden-hover.png',
-//         '/play.png',
-//         '/github.png',
-//         '/twitter.png',
-//         '/mail.png',
-//         '/more.png',
-//         '/slide-thick.png'
-//     ]
+    const images = [
+        '/experiences.png',
+        '/about.png',
+        '/about-hover.png',
+        '/project.png',
+        '/project-hover.png',
+        '/anglerfish.png',
+        '/anglerfish-hover.png',
+        '/garden.png',
+        '/garden-hover.png',
+        '/play.png',
+        '/github.png',
+        '/twitter.png',
+        '/mail.png',
+        '/more.png',
+        '/slide-thick.png'
+    ]
 
-//     const promises = images.map((src) => {
-//       return new Promise((resolve, reject) => {
-//         const img = new Image();
-//         img.src = src;
-//         img.onload = resolve;
-//         img.onerror = reject;
-//       });
-//     });
+    const promises = images.map((src) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = resolve;
+        img.onerror = reject;
+      });
+    });
 
-//     Promise.all(promises)
-//       .then(() => setLoading(false))
-//       .catch((err) => console.error(err));
-//   }, []);
+    Promise.all(promises)
+      .then(() => setLoading(false))
+      .catch((err) => console.error(err));
+  }, []);
 
     return (
         <DndProvider backend={MultiBackend} options={HTML5toTouch}>
