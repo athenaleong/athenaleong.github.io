7be6d9f1e604ba022c5f3382e88e106b2fa286f2
athenaleong <athenaleong619@gmail.com>
2023-04-08 00:06:00 -0700

    Removed vite skeleton file

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import Desktop from './container/Desktop'
 import './index.css'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import { DndProvider } from 'react-dnd'
 import { Provider } from 'react-redux'
 import store from './slices'
 import DeviceRouter from './container/DeviceRouter'
-import { TouchBackend } from 'react-dnd-touch-backend'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
