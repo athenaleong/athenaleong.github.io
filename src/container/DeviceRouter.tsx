034d3708fb817f0cf99b5a6093eaa9d829b31152
athenaleong <athenaleong619@gmail.com>
2023-04-07 18:50:38 -0700

    Added preloading

--- src/container/DeviceRouter.tsx
+++ src/container/DeviceRouter.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useMediaQuery } from "react-responsive";
 import Desktop from './Desktop';
 import { DndProvider } from 'react-dnd'
@@ -12,11 +12,45 @@ const DeviceRouter: React.FC<any> = () => {
     const isTablet = useMediaQuery({ query: "(min-width: 768px) and (max-width: 991px)" });
     const isLaptop = useMediaQuery({ query: "(min-width: 992px)" });
 
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+    const images = [
+        '/src/assets/experiences.png',
+        '/src/assets/about.png',
+        '/src/assets/about-hover.png',
+        '/src/assets/project.png',
+        '/src/assets/project-hover.png',
+        '/src/assets/anglerfish.png',
+        '/src/assets/anglerfish-hover.png',
+        '/src/assets/garden.png',
+        '/src/assets/garden-hover.png',
+        '/src/assets/play.png',
+        '/src/assets/github.png',
+        '/src/assets/twitter.png',
+    ]
+
+    const promises = images.map((src) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = resolve;
+        img.onerror = reject;
+      });
+    });
+
+    Promise.all(promises)
+      .then(() => setLoading(false))
+      .catch((err) => console.error(err));
+  }, []);
+
     return (
         <DndProvider backend={MultiBackend} options={HTML5toTouch}>
-            {isMobile && <Mobile />}
-            {isTablet && <Desktop />}
-            {isLaptop && <Desktop />}
+            {loading && <div> LOADING </div>}
+            {!loading && isMobile && <Mobile />}
+            {!loading && isTablet && <Desktop />}
+            {!loading && isLaptop && <Desktop />}
         </DndProvider>
     )
 
--- src/container/WhatTab.tsx
+++ src/container/WhatTab.tsx
@@ -15,7 +15,7 @@ const WhatTab: React.FC<any> = ({isMobile = false}: {
         I strive to live with everlasting exuberance.
     </p>
     <p className="mb-6 leading-loose">
-        I love skipping down the streets; when I'm happy, I dance with abandon. I find our universe deeply fascinating - how cool is it that billions of years of happenings have led to life as we know it. I bathe in this wonder by learning broadly and deeply. I learn by making, and I hope to share my wonder through the projects I work on!
+        I love skipping down the streets; when I'm happy, I dance with abandon. I find our universe deeply fascinating - how cool is it that billions of years of happenings have led to life as we know it. I bathe in this wonder. I learn by making, and I hope to share my wonder through the projects I work on!
     </p>
     <p className="mb-6 leading-loose">
         I had a rather unconventional childhood. I started boarding school when I was 6 and grew up fiercely independent. I fell in love with science and coding when I was 13. I spent all of my teen wrangling the Singapore education system so I could do more of what I love. This resulted in many breaks spent hacking in the restroom and holidays spent in the science lab. 
@@ -27,7 +27,7 @@ const WhatTab: React.FC<any> = ({isMobile = false}: {
         <img src='/src/assets/experiences.png' className='w-[70%]'/>
     </div>
     <p className="mb-6 leading-loose">
-        I care deeply about the journey. I am most grateful for my friends that traverse this journey with me. Their authentic presence and zest for life inspire me every day. They are like anglerfish! They show me everlasting exuberance whenever life gets unimaginably dark. I want to this journey to be true to myself and my values, with an abundance of joy, wide-eyed curiosity, and thoughtfulness!
+        I care deeply about the journey and I am most grateful for my friends who traverse this journey with me. Their authentic presence and zest for life inspire me every day. They are like anglerfish! They show me everlasting exuberance in moments of darkness. I want to this journey to be true to myself and my values, with an abundance of joy, wide-eyed curiosity, thoughtfulness and anglerfish!
     </p>
     <p className="mb-6 leading-loose">
     </p>
